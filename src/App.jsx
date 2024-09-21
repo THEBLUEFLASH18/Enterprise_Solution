5f1668b5c6985b8fcdf87587938e085039bd105b
THEBLUEFLASH18 <68237204+THEBLUEFLASH18@users.noreply.github.com>
2024-09-21 14:24:01 -0400

    Added Nav bar version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +8,19 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="nav">
+          <div className="logo">
+            <img src='../public/Enterprise_SolutionsV3.png' alt="Logo" />
+          </div>
+          <div>
+            <ul>
+              <li><a href='#About'>About</a></li>
+              <li><a href='#Values'>Values</a></li>
+              <li><a href='#Services'>Services</a></li>
+            </ul>
+          </div>
       </div>
-      <h1>Marcos React app</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
     </>
   )
 }
